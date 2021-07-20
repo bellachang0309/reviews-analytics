0bc7034e2424119eccff92a45a46bb3397cd82f7
Bella Chang <91xa14@ms.mingdao.edu.tw>
2021-07-20 17:15:18 +0800

    caculate avg len of reviews

--- read.py
+++ read.py
@@ -6,11 +6,10 @@ with open('reviews.txt','r') as f:
 		count+=1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0].strip())
-print('.....................................')
-print(data[1])
 
 
-
-
+sum_len=0
+for d in data:
+	sum_len+=len(d)
+print(sum_len)
+print('留言的平均長度為',sum_len/len(data))
